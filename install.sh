76403a84898c50b2f0c202cd61312f515a3f8ef2
AlexanderSpirin <AlexanderSpirin@users.noreply.github.com>
2017-03-02 15:32:19 +0300

    Fix directory change problem

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ chsh -s /bin/zsh
 
 # install command-line fuzzy finder
 if [ -d "${HOME}/.fzf" ]; then
-  cd ${HOME}/.fzf && git pull && ./install
+  (cd ${HOME}/.fzf && git pull && ./install)
 else
   git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf && ${HOME}/.fzf/install
 fi
