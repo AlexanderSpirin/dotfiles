ca0a1db7d9b3be122065d585a42624f8d840b648
AlexanderSpirin <AlexanderSpirin@users.noreply.github.com>
2017-03-02 15:18:50 +0300

    Add .ackrc, .screenrc, .zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install git vim zsh
+sudo apt-get install screen git vim zsh
 
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   sh -c "$(wget https://raw.githubusercontent.com/AlexanderSpirin/oh-my-zsh/master/tools/install.sh -O -)"
@@ -10,8 +10,12 @@ fi
 chsh -s /bin/zsh
 
 # install command-line fuzzy finder
-if [ -d "${HOME}/.fzf" ]; then 
+if [ -d "${HOME}/.fzf" ]; then
   cd ${HOME}/.fzf && git pull && ./install
 else
   git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf && ${HOME}/.fzf/install
 fi
+
+cp .ackrc    ${HOME}/.ackrc
+cp .screenrc ${HOME}/.screenrc
+cp .zshrc    ${HOME}/.zshrc
