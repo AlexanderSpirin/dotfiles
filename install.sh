7e98daf930e09846d44843c4a99ba2b4c37458c4
AlexanderSpirin <AlexanderSpirin@users.noreply.github.com>
2017-03-02 15:07:35 +0300

    Add zsh and oh-my-zsh setup

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+sudo apt-get install git vim zsh
+
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/AlexanderSpirin/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+
+# change shell
+chsh -s /bin/zsh
+
 # install command-line fuzzy finder
 if [ -d "${HOME}/.fzf" ]; then 
   cd ${HOME}/.fzf && git pull && ./install
